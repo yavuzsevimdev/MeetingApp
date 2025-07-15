438a9cd99646b9ab6a7e42ab692a93433f0026b9
yavuzsevimdev <yavuzsevimdev@gmail.com>
2025-07-15 15:19:07 +0300

    İlk Yükleme: MeetingApp projesi oluşturuldu. Controller, View ve Layoutlar tanımlandı. Genel ayarlamalar yapıldı

---
+++ Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+
+//{controller=Home}/{action=Index}/id?
+//app.MapDefaultControllerRoute();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+
+
+app.Run();
